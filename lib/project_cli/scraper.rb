9b32f8adce911013a0e0c2f1d65846911c2ade09
chic-page-6502 <zacharytq@gmail.com>
2019-10-13 02:11:42 +0000

    working on trails method

--- lib/project_cli/scraper.rb
+++ lib/project_cli/scraper.rb
@@ -5,11 +5,23 @@ class ProjectCli::Scraper
   end
   
   def self.trail_urls
-    doc = Nokogiri::HTML(open("https://www.mtbproject.com/directory/8007797/central-florida"))
-    doc.search(".trail-table .trail-row a").map {|i| i.attribute("href").value}
+    output = self.scrape_region.map do |region|
+      doc = Nokogiri::HTML(open(region))
+      doc.search(".trail-table .trail-row a").map {|i| i.attribute("href").value}
+    end
+    output.flatten
   end
   
   def self.trails
-    self.trail_urls
+    self.trail_urls.map do |trail|
+      doc = Nokogiri::HTML(open(trail))
+      output = {}
+      output[:name] = doc.search("#trail-title").text.strip
+      output[:county] = doc.search("ol.breadcrumb li.breadcrumb-item a span.hidden-sm-down")[1].text
+      output[:difficulty] = doc.search("div.trail-subheader div.difficulty-banner span.difficulty-text").text
+      output
+    end
+    #doc = Nokogiri::HTML(open("https://www.mtbproject.com/trail/7008967/gatorback"))
+    #binding.pry
   end
 end
